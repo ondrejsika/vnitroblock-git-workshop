e27e2b3696b4c36f6b131ad48c1a3562538578ed
Ondrej Sika <ondrej@ondrejsika.com>
2019-06-28 11:07:49 +0200

    add primer desing style

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
-import Hello from '../compponents/Hello';
+import Hello from "../components/Hello";
+import { Box, Button, Heading, Text, BaseStyles } from "@primer/components";
 
 function Home() {
-  return <div>
-      <Hello name='Zuzko' />
-      <br />
-      <a href='/about-me'>About me</a>
-    </div>;
+  return (
+    <>
+      <BaseStyles>
+        <Box m={4}>
+          <Heading mb={2}>Hello, world!</Heading>
+          <p>This will get Primer text styles.</p>
+          <Hello name="Zuzko" />
+          <br />
+          <a href="/about-me">About me</a>
+        </Box>
+      </BaseStyles>
+    </>
+  );
 }
 
 export default Home;
