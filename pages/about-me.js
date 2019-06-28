7b2d3120ade80211104185ad75b7d809ed6fdd6c
Ondrej Sika <ondrej@ondrejsika.com>
2019-06-28 10:31:40 +0200

    add pages

---
+++ pages/about-me.js
@@ -0,0 +1,8 @@
+function AboutMe() {
+  return <div>
+    <h1>Ondrej Sika</h1>
+    <p>I am a software engineer form Prague</p>
+  </div>;
+}
+
+export default AboutMe;
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home() {
+  return <div>
+      Welcome to my React Workshop!
+      <br />
+      <a href='/about-me'>About me</a>
+    </div>;
+}
+
+export default Home;
